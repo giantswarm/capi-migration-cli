2239e0ba233549549357fc55812edbec53102b8d
Berk Dehrioglu <berk@giantswarm.io>
2023-12-18 14:02:08 +0300

    add additional IRSA domains

--- pkg/migrator/capi_app.go
+++ pkg/migrator/capi_app.go
@@ -172,6 +172,7 @@ func (s *Service) generateClusterConfigData(ctx context.Context) (*ClusterAppVal
 					"initial-cluster":                                "$ETCD_INITIAL_CLUSTER",
 					"experimental-peer-skip-client-san-verification": "true",
 				},
+				IrsaAdditionalDomain: fmt.Sprintf("https://irsa.%s.k8s.%s.%s.aws.gigantic.io", s.clusterInfo.Name, s.clusterInfo.MC.VintageMC, s.vintageCRs.AwsCluster.Spec.Provider.Region),
 			},
 		},
 	}
